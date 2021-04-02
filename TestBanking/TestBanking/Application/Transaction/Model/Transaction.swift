3af238131a9ff9c2882299d8158565c8b27fe816
Anshika <kalra.anshika@gmail.com>
2021-04-02 14:09:13 +0530

    [UserStory2] Transaction Model

---
+++ TestBanking/TestBanking/Application/Transaction/Model/Transaction.swift
@@ -0,0 +1,49 @@
+//
+//  Transaction.swift
+//  TestBanking
+//
+//  Created by Anshika on 02/04/21.
+//
+
+import Foundation
+
+enum ProcessingStatus {
+    case posted
+    case pending
+    case unKnown(value: String)
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let productName = try? container.decode(String.self)
+        switch productName {
+        case "POSTED":
+            self = .posted
+        case "PENDING":
+            self = .pending
+        default:
+            self = .unKnown(value: productName ?? "unKnown")
+        }
+    }
+    
+    func description() -> String{
+        switch self {
+        case .posted:
+            return "Saving"
+        case .pending:
+            return "Spending"
+        default:
+            return "Unknown"
+        }
+    }
+}
+
+struct Transactions {
+    var transactions: [Transaction]
+}
+
+struct Transaction {
+    let date: String
+    let description: String
+    let amount: Double
+    let runningBalance: Double
+    let processingStatus: ProcessingStatus
+}
